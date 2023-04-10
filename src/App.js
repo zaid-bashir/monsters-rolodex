e7d0137ed4388fc25940e559c13159f65c4db359
zaidbashir <zaidwani2282@gmail.com>
2023-04-10 12:49:59 +0530

    Simple React Class Component With two SetState Approaches.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
+import { Component } from 'react';
 import './App.css';
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello! Welcome To React App Simple Default Functional Component
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/zaid-bashir/monsters-rolodex"
-          target="_blank"
-          rel="Visit Github Repo of this Project noreferrer"
-        >
-          Visit Github Repo of this Project
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  //* Creating Constructor and Initialising Variables.
+  constructor(){
+    super();
+    this.state = {
+      name : "Zaid Bashir"
+    };
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            Hello! Welcome To React App Simple React Class Components
+          </h1>
+
+          {/* Accessing Variables In Curly Braces.*/}
+
+          <h2>Welcome {this.state.name}</h2>
+
+          {/* Using Button To Fire OnClick Events.*/}
+
+          <button onClick={() => {
+             //* Not Optimal Way for SetState Use.
+             //* By this Way the State is not Up to Date After Click Action. It gets Updated After 2nd Click
+            // this.setState({name : "Coding Rockers"})
+            // console.log(this.state);
+
+            //* By this Way the State is Up to Date After Every Click Action
+            this.setState(
+              () => {
+                return {name : "Coding Rockers"};
+              },
+              //This Block is Optional
+              () => {
+                console.log(this.state);
+              }
+            );
+          }} >Click Here</button>
+
+          <br/>
+
+          <a
+            className="App-link"
+            href="https://github.com/zaid-bashir/monsters-rolodex"
+            target="_blank"
+            rel="Visit Github Repo of this Project noreferrer"
+          >
+            Visit Github Repo of this Project
+          </a>
+        </header>
+      </div>
+    );
+  }
+
 }
+
 
 export default App;
