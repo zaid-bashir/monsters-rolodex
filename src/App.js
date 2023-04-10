756c1ecefbaa76c1d15c72b41e3b8b5677ed9a59
Zaid Bashir <zaidbashir@Zaids-MacBook-Pro.local>
2023-04-10 11:42:34 +0530

    Simple Default Functional Components

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello! Welcome To React App Simple Default Functional Component
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/zaid-bashir/monsters-rolodex"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="Visit Github Repo of this Project noreferrer"
         >
-          Learn React
+          Visit Github Repo of this Project
         </a>
       </header>
     </div>
